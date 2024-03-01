30a43554cd75f9524279ea0e839c0b74cfaabfc6
Saadia Saleem <154665738+saadiasaleem67@users.noreply.github.com>
2024-03-02 00:05:18 +0500

    Add files via upload

---
+++ oop.js
@@ -0,0 +1,47 @@
+import inquirer from "inquirer";
+class Student {
+    constructor(name) {
+        this.name = name;
+    }
+}
+class Person {
+    constructor() {
+        this.object = [];
+    }
+    addStudent(obj) {
+        this.object.push(obj);
+    }
+}
+const persons = new Person();
+const programStart = async (persons) => {
+    do {
+        let answer = await inquirer.prompt({
+            type: "list",
+            name: "user",
+            message: "What you want to:",
+            choices: ["Project", "Talk to Friend",]
+        });
+        if (answer.user == "Project") {
+            console.log("I have completed all my Typescript Project.... \n");
+        }
+        if (answer.user == "Talk to Friend") {
+            let again = await inquirer.prompt({
+                type: "input",
+                name: "again",
+                message: "Enter your Friend Name\n"
+            });
+            const name1 = persons.object.find(val => val.name == again.again);
+            if (!name1) {
+                const newstudent = new Student(again.again);
+                persons.addStudent(newstudent);
+                console.log(`Hello ${newstudent.name} , Did you learn Typescript?\n`);
+                console.log(persons.object);
+            }
+            if (name1) {
+                console.log(`Congratulation ${name1.name}, for completed all Typescript project....\n`);
+                console.log(persons.object);
+            }
+        }
+    } while (true);
+};
+programStart(persons);
